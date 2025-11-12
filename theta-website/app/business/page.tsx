f3d6e654c91ee3fbe63846500217d065215aecc7
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-11-12 11:37:43 +0000

    more work on the business page

--- theta-website/app/business/page.tsx
+++ theta-website/app/business/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import {Header} from "@/components/header";
+import { Button } from "@/components/ui/button";
+import { InfoIcon, SquareArrowOutUpRightIcon } from "lucide-react";
 
 export default function Home() {
   return (
     <div>
       <BusinessPageIntro />
+      <EnrollmentInfoSection />
     </div>
   );
 }
@@ -13,8 +16,28 @@ function BusinessPageIntro() {
     return (
         <div className="page-intro-section">
             <div className="page-intro-section-inner">
-                <div className="page-intro-subtitle-upper">ThetaOS Business</div>
-                <div className="page-intro-title">Simple, powerful fleet management with Grid</div>
+                <div className="page-intro-subtitle-upper">ThetaOS Business + Quntem Grid</div>
+                <div className="page-intro-title">Simple, powerful fleet management with ThetaOS</div>
+                <div className="page-intro-subtitle-lower">Control devices, deploy software, and monitor your fleet with ease</div>
+                <div className="page-intro-button-row">
+                  <Button variant="outline"><SquareArrowOutUpRightIcon /> Get Started</Button>
+                  <Button variant="outline"><InfoIcon /> Learn More</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+function EnrollmentInfoSection() {
+    return (
+        <div className="page-info-section">
+            <div className="page-info-section-inner">
+                <div className="page-info-section-content">
+                  <div className="page-info-section-subtitle">Enrollment</div>
+                  <div className="page-info-section-title">Effortless Self-Enrollment</div>
+                  <div className="page-info-section-text">Users self-enroll their devices with a Managed Quntem Account, no IT work required</div>
+                </div>
+                <img src="/self-enrollment.png" className="page-info-section-image" />
             </div>
         </div>
     )
