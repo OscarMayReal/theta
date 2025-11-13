6ae16aa0a4f807879648efc2c2e0e430942c0eca
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-11-13 10:10:48 +0000

    more website upgrades

--- theta-website/app/business/page.tsx
+++ theta-website/app/business/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
   return (
     <div>
       <BusinessPageIntro />
+      <LargeScaleDeploymentInfoSection />
       <EnrollmentInfoSection />
     </div>
   );
@@ -38,6 +39,35 @@ function EnrollmentInfoSection() {
                   <div className="page-info-section-text">Users self-enroll their devices with a Managed Quntem Account, no IT work required</div>
                 </div>
                 <img src="/self-enrollment.png" className="page-info-section-image" />
+            </div>
+        </div>
+    )
+}
+
+function LargeScaleDeploymentInfoSection() {
+    return (
+        <div className="page-info-section">
+            <div className="page-info-section-inner" style={{
+              flexDirection: "column"
+            }}>
+                <div className="page-info-section-content" style={{
+                  alignItems: "center",
+                  justifyContent: "center"
+                }}>
+                  <div className="page-info-section-subtitle" style={{
+                    textAlign: "center"
+                  }}>Scales Easily</div>
+                  <div className="page-info-section-title" style={{
+                    textAlign: "center"
+                  }}>ThetaOS scales from 1 device to thousands</div>
+                  <div className="page-info-section-text" style={{
+                    textAlign: "center"
+                  }}>Deploy ThetaOS to any number of devices with ease, and monitor, manage, and control them all from one, easy-to-use interface</div>
+                </div>
+                <img src="/fleet.png" style={{
+                  width: "100%",
+                  marginTop: "30px"
+                }} />
             </div>
         </div>
     )
